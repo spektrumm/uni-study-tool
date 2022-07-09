dc7c91efba33b1cadd7ffebce5371aceeed7d3f4
spektrumm <51189697+spektrumm@users.noreply.github.com>
2022-07-09 15:12:56 -0700

    cleanup file directory, rename files

---
+++ vanillaJS/src/jsonIO.js
@@ -0,0 +1,24 @@
+const fs = require("fs");
+const fileName = "./vanillaJS/src/data/previousLists.json";
+
+function readJson(fileName) {
+    fs.readFile(fileName, "utf8", (err, jsonString) => {
+        if (err) {
+          console.log("File read failed:", err);
+          return;
+        }
+        console.log("File data:", jsonString);
+        return jsonString;
+      });
+}
+
+function writeJson(fileName, jsonString) {
+    fs.writeFile(fileName, jsonString, (err) => {
+        if (err) {
+          console.log("File write failed:", err);
+          return;
+        }
+        console.log("List Item saved to file");
+      });
+}
+
